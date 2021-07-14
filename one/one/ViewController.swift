f4f52e60c75ed919bc42049f893154d3319c1411
邓江洲 <dengjiangzhouReal@gmail.com>
2021-07-14 09:29:51 +0800

    do start

---
+++ one/one/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  one
+//
+//  Created by Jz D on 2021/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.blue
+    }
+
+
+}
+
---
+++ one/one/ViewControllerTwo.swift
@@ -0,0 +1,36 @@
+//
+//  ViewControllerTwo.swift
+//  one
+//
+//  Created by Jz D on 2021/7/14.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.systemPink
+        
+        
+        
+    }
+    
+
+    
+    @IBAction func gg(_ sender: Any) {
+        
+        
+        dismiss(animated: true) {
+            
+        }
+        
+    }
+    
+    
+
+}
